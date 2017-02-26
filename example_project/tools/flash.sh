7249abeb2d115889637bd0194a4fa628f2f46a8b
Mark Konnov <markkonnov@gmail.com>
2017-02-27 00:07:33 +0300

    flash tool

---
+++ example_project/tools/flash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function flash_dfu {
+	dfu-util --device , -a 0 -s "$2" -D "$1"
+}
+
+function flash_dfu_leave {
+	dfu-util --device , -a 0 -s "$2":leave -D "$1"
+}
+
+flash_dfu_leave partitions.bin 0x08000000
