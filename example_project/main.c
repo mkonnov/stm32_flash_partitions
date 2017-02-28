36fe2d59694f12d0a350d41f09c75987f0ee1135
Mark Konnov <markkonnov@gmail.com>
2017-02-28 14:00:54 +0300

    simple initial test provided

--- example_project/main.c
+++ example_project/main.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "stm32f10x.h"
 #include "system_stm32f10x.h"
-#include "stm32f10x_gpio.h"
-
 #include "uart.h"
 #include "printf.h"
+#include "partitions.h"
+
+#include "flash.h"
 
 static void init_rcc(void) {
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN | RCC_APB1ENR_BKPEN;
@@ -25,7 +27,35 @@ int main(void) {
 	uart_init();
 	init_printf(0, putc);
 
-	printf("Hello\r\n");
+	prtn_callbacks_t cb = {
+		flash_write_block,
+		flash_erase_page,
+		flash_unlock,
+		flash_lock,
+		__disable_irq,
+		__enable_irq
+	};
+
+	prtn_table_t prtns = {
+		{2048, 0x08000000, 1024 * 256},
+		{
+			{"bootloader", 0x08000000, 62 * 1024},
+			{"firmware",   0x0800f800, 40 * 1024},
+			{"settings", 0xdeadbeef, 62 * 1024},
+			{NULL, NULL, NULL}
+		}
+	};
+	
+	partitions_register_callbacks(&cb);
+	partition_table_init(&prtns);
+	
+	int i = 0;
+	while (prtns.partitions[i].origin_addr != NULL) {
+		printf("----\n\r%s\n\r", prtns.partitions[i].name);
+		printf("origin: %02X\r\n", prtns.partitions[i].origin_addr);
+		printf("size: %d\r\n", prtns.partitions[i].size);
+		i++;
+	}
 
 	for(;;);
 	return 0;
